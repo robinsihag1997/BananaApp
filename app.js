b72121f51124a55d7622dce29ccdae5be2908477
robinsihag1997 <robinsihag22@gmail.com>
2023-02-01 13:26:06 +0530

    final commit of app

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-let textarea=document.querySelector("#textArea");
-let textarea2=document.querySelector("#textArea2");
+let inputValue=document.querySelector("#textArea");
+let outputValue=document.querySelector("#textArea2");
 
 let btn=document.querySelector("#btn");
 
+// let url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+let url="https://api.funtranslations.com/translate/minion.json"
+
+
+
 btn.addEventListener("click",()=>{
-   valueChanged();
+   valueChanged(inputValue.value);
     emptyValue();
 });
 
 
 function valueChanged(para){
-    textarea2.textContent=textarea.value;
+    let completeUrl=url+"?"+"text="+para;
+    fetch(completeUrl)
+    .then(response=>response.json())
+    // .then(data=>console.log(data.contents.text))
+    .then(data=>outputValue.textContent=data.contents.translated)
+    .catch(error=>alert(`error occurred please try again : ${error}`));
+
+
+
+    // outputValue.textContent=inputValue.value;
 }
 
 function emptyValue(){
-    textarea.value=" ";
+    inputValue.value=" ";
 }
+
